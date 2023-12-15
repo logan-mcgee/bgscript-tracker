218d052ceff9268f6c1728354243e60f36d545a8
logan <31740751+logan-mcgee@users.noreply.github.com>
2023-12-15 13:43:16 +0000

    update starting version

--- src/scanv2.js
+++ src/scanv2.js
@@ -4,7 +4,7 @@ import path from 'node:path';
 import { request } from 'undici';
 import crypto from 'node:crypto';
 
-const STARTING_VERSION = 3028;
+const STARTING_VERSION = 3095;
 const ENDING_VERSION = STARTING_VERSION + 100;
 
 function main() {
